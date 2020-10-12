6e114bed2c8b31090643be6f408ee602dd4ca335
andrey <andreiluka@ya.ru>
2020-10-12 11:06:07 +0300

    edit menu-accordeon js

--- js/index_js.js
+++ js/index_js.js
@@ -1,25 +1,79 @@
-// pagination
+// nav-fullscreen
 
-var pageItem = document.querySelector('.pagination__item');
-var pageLink = document.querySelector('#page');
+var navFullscreen = document.querySelector('.nav-fullscreen');
 
-pageLink.addEventListener('click', function () {
-   console.log('go');
-   pageItem.classList.add('pagination__item--active');
-});
+function navFullscreenAdd() {
+   navFullscreen.classList.add('nav-fullscreen--active');
+};
+
+function navFullscreenRemove() {
+   navFullscreen.classList.remove('nav-fullscreen--active');
+}
+
+
+var hamburgerMenu = document.querySelector('.hamburger-menu');
+hamburgerMenu.addEventListener('click', navFullscreenAdd);
+
+var navFullscreenCross = document.querySelector('.nav-fullscreen__cross');
+navFullscreenCross.addEventListener('click', navFullscreenRemove);
+
+
+var navLinkBest = document.querySelector('#link-best');
+navLinkBest.addEventListener('click', navFullscreenRemove);
+
+var navLinkProduct = document.querySelector('#link-product');
+navLinkProduct.addEventListener('click', navFullscreenRemove);
+
+var navLinkTeam = document.querySelector('#link-team');
+navLinkTeam.addEventListener('click', navFullscreenRemove);
+
+var navLinkMenu = document.querySelector('#link-menu');
+navLinkMenu.addEventListener('click', navFullscreenRemove);
+
+var navLinkReviews = document.querySelector('#link-reviews');
+navLinkReviews.addEventListener('click', navFullscreenRemove);
+
+var navLinkHow = document.querySelector('#link-how');
+navLinkHow.addEventListener('click', navFullscreenRemove);
+
+var navLinkMap = document.querySelector('#link-map');
+navLinkMap.addEventListener('click', navFullscreenRemove);
+
+
+
+// var navLink = document.querySelector('nav__link');
+
+// navLink.addEventListener('click', function () {
+//    navFullscreen.classList.remove('nav-fullscreen--active');
+// });
+
+// var navI = document.querySelector('#nav__i');
+
+// navI.addEventListener('click', function (e) {
+//    if (e.target && e.target.matches('a.nav__link')) {
+//       // console.log('работает');
+//       navFullscreenRemove();
+//    }
+// });
 
 
 // menu-accordeon
 
-var accoItem = document.querySelector('.menu-accordeon__item');
+var i = 0;
+
+function itemActiveRemove() {
+   accoItemMeat.classList.remove('menu-accordeon__item--active');
+   accoItemVegan.classList.remove('menu-accordeon__item--active');
+   accoItemDiet.classList.remove('menu-accordeon__item--active');
+   i = 0;
+}
 
 var accoItemMeat = document.querySelector('#accoItemMeat');
 var accoLinkMeat = document.querySelector('.menu-accordeon__link--meat-eater');
 
-var i = 0;
 document.getElementsByClassName('menu-accordeon__link--meat-eater')[0].onclick = function () {
-   // return ++i;
-   console.log(++i);
+   return ++i;
+   // console.log(++i);
 }
 
 accoLinkMeat.addEventListener('click', function (e) {
@@ -28,21 +82,17 @@ accoLinkMeat.addEventListener('click', function (e) {
    if (i < 2) {
       accoItemMeat.classList.add('menu-accordeon__item--active');
    } else {
-      accoItemMeat.classList.remove('menu-accordeon__item--active');
-      accoItemVegan.classList.remove('menu-accordeon__item--active');
-      accoItemDiet.classList.remove('menu-accordeon__item--active');
-      i = 0;
+      itemActiveRemove();
    }
 });
-
 
 
 var accoItemVegan = document.querySelector('#accoItemVegan');
 var accoLinkVegan = document.querySelector('.menu-accordeon__link--vegetarian');
 
 document.getElementsByClassName('menu-accordeon__link--vegetarian')[0].onclick = function () {
-   // return ++i;
-   console.log(++i);
+   return ++i;
+   // console.log(++i);
 }
 
 accoLinkVegan.addEventListener('click', function (e) {
@@ -51,21 +101,17 @@ accoLinkVegan.addEventListener('click', function (e) {
    if (i < 2) {
       accoItemVegan.classList.add('menu-accordeon__item--active');
    } else {
-      accoItemVegan.classList.remove('menu-accordeon__item--active');
-      accoItemMeat.classList.remove('menu-accordeon__item--active');
-      accoItemDiet.classList.remove('menu-accordeon__item--active');
-      i = 0;
+      itemActiveRemove();
    }
 });
-
 
 
 var accoItemDiet = document.querySelector('#accoItemDiet');
 var accoLinkDiet = document.querySelector('.menu-accordeon__link--dietary');
 
 document.getElementsByClassName('menu-accordeon__link--dietary')[0].onclick = function () {
-   // return ++i;
-   console.log(++i);
+   return ++i;
+   // console.log(++i);
 }
 
 accoLinkDiet.addEventListener('click', function (e) {
@@ -74,9 +120,19 @@ accoLinkDiet.addEventListener('click', function (e) {
    if (i < 2) {
       accoItemDiet.classList.add('menu-accordeon__item--active');
    } else {
-      accoItemDiet.classList.remove('menu-accordeon__item--active');
-      accoItemMeat.classList.remove('menu-accordeon__item--active');
-      accoItemVegan.classList.remove('menu-accordeon__item--active');
-      i = 0;
+      itemActiveRemove();
    }
+});
+
+
+
+
+// pagination
+
+var pageItem = document.querySelector('.pagination__item');
+var pageLink = document.querySelector('#page');
+
+pageLink.addEventListener('click', function () {
+   console.log('go');
+   pageItem.classList.add('pagination__item--active');
 });
