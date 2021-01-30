f2cb32693026e1c2b7ebe739d11f2efed2dd3671
andrey <andreiluka@ya.ru>
2021-01-30 13:40:06 +0300

    map without clusterer

--- js/main.js
+++ js/main.js
@@ -471,7 +471,6 @@ var placemarks = [
       hintContent: '<div class="map__hint">ул. Литераторов, д.19</div>',
       balloonContent: [
          '<div class="map__balloon">',
-         // '<img class="map__balloon-img" scr="../img/logo.png" alt="Бургер"/>',
          'Работаем с 10:00 до 19:00, без выходных. ',
          'Вход со двора',
          '</div>'
@@ -501,7 +500,7 @@ var placemarks = [
    }
 ]
 
-var geoObjects = [];
+// var geoObjects = [];
 
 function init() {
    var map = new ymaps.Map('map_9', {
@@ -511,33 +510,49 @@ function init() {
       behaviors: ['drag']
    });
 
-   for (var i = 0; i < placemarks.length; i++) { 
-      
-      geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
-         hintContent: placemarks[i].hintContent,
-         balloonContent: placemarks[i].balloonContent.join('')
+   placemarks.forEach(function(obj){
+      var placemark = new ymaps.Placemark([obj.latitude, obj.longitude], {
+         hintContent: obj.hintContent,
+         balloonContent: obj.balloonContent.join('')
       }, 
       {
          iconLayout: 'default#image',
-         iconImageHref: '../../img/map-marker.png',
+         iconImageHref: '../img/map-marker.png',
          iconImageSize: [46, 57],
          iconImageOffset: [-23, -57]
       });
-   }
 
-   var clusterer = new ymaps.Clusterer({
-      clusterIcons: [
-         {
-            href: '../../img/dark-burger.png',
-            size: [50, 40],
-            offset: [-25, -20]
-         }
-      ],
-      clusterIconContentLayout: null
+      map.geoObjects.add(placemark);
    });
+
+   // for (var i = 0; i < placemarks.length; i++) { 
+      
+   //    geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude], {
+   //       hintContent: placemarks[i].hintContent,
+   //       balloonContent: placemarks[i].balloonContent.join('')
+   //    }, 
+   //    {
+   //       iconLayout: 'default#image',
+   //       iconImageHref: '../img/map-marker.png',
+   //       iconImageSize: [46, 57],
+   //       iconImageOffset: [-23, -57]
+   //    });
+   // }
+
+   // var clusterer = new ymaps.Clusterer({
+   //    clusterIcons: [
+   //       {
+   //          href: '../img/dark-burger.png',
+   //          size: [50, 40],
+   //          offset: [-25, -20]
+   //       }
+   //    ],
+   //    clusterIconContentLayout: null
+   // });
    
-   map.geoObjects.add(clusterer);
-   clusterer.add(geoObjects);
+
+   // map.geoObjects.add(clusterer);
+   // clusterer.add(geoObjects);
 }
 
 
