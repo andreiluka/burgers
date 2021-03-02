14a816ed6f676765eae8f339df5e36472b425309
andrey <andreiluka@ya.ru>
2021-03-02 19:58:40 +0300

    using Gulp

---
+++ src/js/products-slider.js
@@ -0,0 +1,25 @@
+;(function() {
+
+   $(document).ready(function () {
+
+      var slider = $('.products__list').bxSlider({
+         controls: false,
+         pager: false,
+         slideMargin: 50,
+         swipeThreshold: 200
+      });
+   
+      $('.products-slider__arrow-left').on('click', function (e) {
+         e.preventDefault();
+   
+         slider.goToPrevSlide();
+      });
+   
+      $('.products-slider__arrow-right').on('click', function (e) {
+         e.preventDefault();
+   
+         slider.goToNextSlide();
+      });
+   });
+   
+})()
